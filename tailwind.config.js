f8ae1a93629e71139eeb40df83b06fe678180234
RonkZeDonk <20400064+RonkZeDonk@users.noreply.github.com>
2023-09-06 08:51:38 -0400

    refactor: revamp website's main template

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@
 module.exports = {
   content: ["./views/**/*.hbs"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          700: "#1D181E",
+          800: "#19141A",
+        },
+        secondary: "#5C4194",
+        accent: "#DD6336",
+      },
+    },
   },
   plugins: [],
-}
-
+};
